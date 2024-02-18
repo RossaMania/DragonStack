396f4e8a86cd9b077e6f040bebd3855ffbbd5278
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-18 14:58:44 -0600

    Move dragon.js into backend folder. Create two Dragon instances, and console log them.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
 const Dragon = require("./dragon.js");
+
+const fooey = new Dragon(new Date(), "fooey");
+
+const baloo = new Dragon(new Date(), "baloo");
+
+console.log("fooey", fooey);
+
+console.log("baloo", baloo);
