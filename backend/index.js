4b8b158bf7308d6963222a54e58a413eb16c2481
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-20 20:59:53 -0600

    Comment out previous code in index.js. Require Generation class for generation const. Create new dragon with generation const calling newDragon const. Console log new dragon. Textg expiration by trying to create dragon 15 seconds after generation date.

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,46 @@
-const Dragon = require("./dragon.js");
+// const Dragon = require("./dragon.js");
 
-const fooey = new Dragon({
-  birthdate: new Date(),
-  nickname: "fooey"
-});
+// const fooey = new Dragon({
+//   birthdate: new Date(),
+//   nickname: "fooey"
+// });
 
-const baloo = new Dragon({
-  birthdate: new Date(),
-  nickname: "baloo",
-  traits: [
-    { traitType: "backgroundColor", traitValue: "green" }
-  ]
-});
+// const baloo = new Dragon({
+//   birthdate: new Date(),
+//   nickname: "baloo",
+//   traits: [
+//     { traitType: "backgroundColor", traitValue: "green" }
+//   ]
+// });
 
 
-const mimar = new Dragon();
+// const mimar = new Dragon();
+
+// setTimeout(() => {
+//   const gooby = new Dragon();
+//   console.log("gooby", gooby);
+// }, 3000);
+
+
+// console.log("fooey", fooey);
+
+// console.log("baloo", baloo);
+
+// console.log("mimar", mimar);
+
+
+
+const Generation = require("./generation.js");
+
+const generation = new Generation();
+
+console.log("generation", generation);
+
+const gooby = generation.newDragon(); // This will throw an error because the generation has expired
+
+console.log("gooby", gooby);
 
 setTimeout(() => {
-  const gooby = new Dragon();
-  console.log("gooby", gooby);
-}, 3000);
-
-
-console.log("fooey", fooey);
-
-console.log("baloo", baloo);
-
-console.log("mimar", mimar);
-
-
-
+  const mimar = generation.newDragon();
+  console.log("mimar", mimar);
+}, 15000);
