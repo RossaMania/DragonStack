d4541182065773164f44ac207a90f7ece5e4c038
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-20 21:16:44 -0600

    Call setTimeout method for buildNewGeneration func call, and timer will be generation expiration in milliseconds with this.generation.expiration.getTime() - Date.now().

--- backend/engine.js
+++ backend/engine.js
@@ -2,12 +2,15 @@ const Generation = require("./generation.js");
 
 class GenerationEngine {
   constructor() {
-    this.generation = null;
+    this.generation = null; // The current generation object
   }
 
   buildNewGeneration() {
     this.generation = new Generation();
 
     console.log("new generation", this.generation);
+
+    setTimeout(() => this.buildNewGeneration(),
+    this.generation.expiration.getTime() - Date.now()); // Time until the generation expires in milliseconds
   }
 }
