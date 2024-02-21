6e60486e314d8868130055f04ca3efdc46d0e62d
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-20 21:10:30 -0600

    In GenerationEngine class create a constructor with a null generation. Create a buildNewGeneration func which calls Generation and console logs "new generation".

--- backend/engine.js
+++ backend/engine.js
@@ -1,5 +1,13 @@
 const Generation = require("./generation.js");
 
 class GenerationEngine {
-  
+  constructor() {
+    this.generation = null;
+  }
+
+  buildNewGeneration() {
+    this.generation = new Generation();
+
+    console.log("new generation", this.generation);
+  }
 }
