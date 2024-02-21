fa3118575a61bc658df4e60effa35c8630df0caf
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-21 16:43:48 -0600

    Refactor route in dragon.js file by replacing app.get with router.get. Export router object in app/api/dragon.js.

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const router = new Router();
 
-app.get("/dragon/new", (req, res) => {
+router.get("/dragon/new", (req, res) => {
   res.json({ dragon: engine.generation.newDragon() });
 }); // Express route web request endpoint to get a new dragon. This will be a GET request to the /dragon/new endpoint.
+
+module.exports = router; // Export the router object..
