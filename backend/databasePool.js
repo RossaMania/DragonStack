9bb7270d5d61bd8e67dec56727cb968ce57bf35e
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-25 17:18:03 -0600

    Create backend/secrets. Add database configuration file to secrets folder. Create database pool in databasePool.js, pass in databaseConfiguration const, and export pool. Add secrets folder to be ignored in gitignore file.

--- backend/databasePool.js
+++ backend/databasePool.js
@@ -1,2 +1,8 @@
 const { Pool } = require("pg");
 
+const databaseConfiguration = require("./secrets/databaseConfiguration.js");
+
+const pool = new Pool(databaseConfiguration);
+
+module.exports = pool;
+
