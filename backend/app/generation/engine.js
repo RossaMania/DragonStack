646aa00029e888298ad76d30ac4aa5fdddf3e415
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-25 20:16:15 -0600

    In storeGeneration func, set this.generation to equal the passed in generation const. Move timer and new generation console log inside the .then block.

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -21,15 +21,22 @@ class GenerationEngine {
 
     GenerationTable.storeGeneration(generation) // Store the generation in the database
     .then(({ generationId }) => {
+      this.generation = generation;
+
       this.generation.generationId = generationId;
+
       console.log("generationId", generationId);
+
+      console.log("new generation", this.generation);
+
+      this.timer = setTimeout(
+        () => this.buildNewGeneration(),
+        this.generation.expiration.getTime() - Date.now()
+      ); // Time until the generation expires in milliseconds
+
     }) // Set generationId & log the generationId to the console
     .catch(error => console.error(error)) // Log the error to the console if there is one
 
-    console.log("new generation", this.generation);
-
-    this.timer = setTimeout(() => this.buildNewGeneration(),
-    this.generation.expiration.getTime() - Date.now()); // Time until the generation expires in milliseconds
   }
 }
 
