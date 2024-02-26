f489de4bd86ac09f296c484b9fbd830e9d8d1f80
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-26 17:03:18 -0600

    Create static storeDragon func in DragonTable. Pass in dragon as argument, and destructure birthdate, nickname, and generationId into a const.

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -1,7 +1,9 @@
 const pool = require("../../databasePool.js");
 
 class DragonTable {
-  
+  static storeDragon(dragon) {
+    const { birthdate, nickname, generationId } = dragon;
+  }
 }
 
 module.exports = DragonTable;
