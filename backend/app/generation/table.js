f16488b65cf37237d39eb5fdd73930a579e0fb13
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-25 19:31:17 -0600

    Update INSERT query to return id in StoreGeneration func.

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -3,7 +3,7 @@ const pool = require("../../databasePool.js");
 class GenerationTable {
   static StoreGeneration(generation) {
     pool.query(
-      'INSERT INTO generation(expiration) VALUES($1)', // $1 is a placeholder for the value of generation.expiration in the array that follows the SQL query string
+      'INSERT INTO generation(expiration) VALUES($1) RETURNING id', // $1 is a placeholder for the value of generation.expiration in the array that follows the SQL query string
       [generation.expiration], // The value of generation.expiration
       (error, response) => {
         if (error) return console.error(error);
