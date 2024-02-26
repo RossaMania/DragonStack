2399d23e141302d7eb867793d4bfa76a235650aa
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-25 19:48:48 -0600

    Call the Promise class in GenerationTable.storeGeneration func. Place pool.query inside Promise call.

--- backend/app/generation/table.js
+++ backend/app/generation/table.js
@@ -2,18 +2,23 @@ const pool = require("../../databasePool.js");
 
 class GenerationTable {
   static storeGeneration(generation) {
-    pool.query(
-      'INSERT INTO generation(expiration) VALUES($1) RETURNING id', // $1 is a placeholder for the value of generation.expiration in the array that follows the SQL query string
-      [generation.expiration], // The value of generation.expiration
-      (error, response) => {
-        if (error) return console.error(error);
 
-        const generationId = response.rows[0].id;
+    return new Promise((resolve, reject) => {
 
-        return generationId;
-      }
+      pool.query(
+        "INSERT INTO generation(expiration) VALUES($1) RETURNING id", // $1 is a placeholder for the value of generation.expiration in the array that follows the SQL query string
+        [generation.expiration], // The value of generation.expiration
+        (error, response) => {
+          if (error) return console.error(error);
+
+          const generationId = response.rows[0].id;
+
+          return generationId;
+        }
       );
-    }
+
+    });
+  }
 }
 
 module.exports = GenerationTable; // Export the GenerationTable class
