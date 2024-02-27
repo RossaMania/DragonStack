5690277aa2ae619f76b9a875a8e35b83d534220c
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-26 19:58:31 -0600

    Refactor code in backend/app/api/dragon.js to create a new dragon instance, then call the storeDragon method from DragonTable object and pass in new dragon as argument. The storeDragon returns a Promise. The .then method is called when the Promise is fulfilled, meaning the dragon is stored in database. The new dragon instance is sent back to client as a JSON response using `res.json({ dragon })`.

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,9 +1,22 @@
 const { Router } = require("express");
+const DragonTable = require("../dragon/table.js"); // Import the DragonTable class from the table.js file in the dragon directory.
 
 const router = new Router();
 
 router.get("/new", (req, res) => {
-  res.json({ dragon: req.app.locals.engine.generation.newDragon() });
-}); // Express route web request endpoint to get a new dragon. This will be a GET request to the /dragon/new endpoint.
+
+  const dragon = req.app.locals.engine.generation.newDragon(); // Get a new dragon instance from the generation engine.
+
+  DragonTable.storeDragon(dragon) // Store the dragon in the database.
+  .then(({ dragonId }) => {
+    console.log('dragonId', dragonId);
+
+    dragon.dragonId = dragonId;
+
+    res.json({ dragon }); // Send the dragon as a JSON response.
+  })
+  .catch(error => console.error(error));
+
+});
 
 module.exports = router; // Export the router object..
