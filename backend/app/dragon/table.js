219c6b5470806adda140c7f175b8d14efe4734e2
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-26 19:26:32 -0600

    Add empty promise to storeDragon func.

--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -3,7 +3,14 @@ const pool = require("../../databasePool.js");
 class DragonTable {
   static storeDragon(dragon) {
     const { birthdate, nickname, generationId } = dragon;
-  }
+
+    
+  return new Promise((resolve, reject) => {
+
+  });
+
+}
+
 }
 
 module.exports = DragonTable;
