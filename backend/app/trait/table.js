6a2e8b38413a673ee8c0eb227d22984cadb2f672
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-29 16:18:04 -0600

    Add getTraitId method to TraitTable class in app/trait/table.js.

--- backend/app/trait/table.js
+++ backend/app/trait/table.js
@@ -1,7 +1,22 @@
 const pool = require("../../databasePool.js");
 
 class TraitTable {
+  static getTraitId = ({ traitType, traitValue }) => {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        'SELECT id FROM trait WHERE "traitType" = $1 AND "traitValue" = $2',
+        [traitType, traitValue],
+        (error, response) => {
+          if (error) return reject(error);
 
+          const traitId = response.rows[0].id;
+
+          resolve({ traitId: traitId });
+        }
+      )
+    });
+
+  }
 }
 
 module.exports = TraitTable; // Export the TraitTable class.
