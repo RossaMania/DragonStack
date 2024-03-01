754fc812f1fc13d8f5bdcfec69dda3ed227f9371
Ross Clettenberg <raclettenberg@gmail.com>
2024-02-29 21:10:36 -0600

    Add method to store dragon traits in app/dragonTrait/table.js. This includes calling getTraitId method from TraitTable, to pass in traitId into query.

--- backend/app/dragonTrait/table.js
+++ backend/app/dragonTrait/table.js
@@ -1,5 +1,24 @@
 const pool = require("../../databasePool.js");
+const TraitTable = require("../trait/table.js");
 
-class DragonTraitTable {}
+class DragonTraitTable {
+  static storeDragonTrait({ dragonId, traitType, traitValue }) {
+
+    return new Promise((resolve, reject) => {
+      TraitTable.getTraitId({ traitType, traitValue })
+      .then(({ traitId }) => {
+        pool.query(
+          `INSERT INTO dragonTrait("traitId", "dragonId")
+          VALUES($1, $2)`,
+          [traitId, dragonId], //Array of values to insert into the SQL query
+          (error, response) => {
+            if (error) return reject(error);
+            resolve(); // No need to return anything, just resolve to end promise
+          }
+      )
+      })
+    });
+  }
+}
 
 module.exports = DragonTraitTable;
