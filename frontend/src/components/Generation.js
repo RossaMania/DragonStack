49dc688aca7278b0e8159b395d4d08201b8b1a3a
Ross Clettenberg <raclettenberg@gmail.com>
2024-03-03 12:03:39 -0600

    feat: add generation info to Generation component and render component in main app.

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -2,6 +2,14 @@ import React from "react";
 
 const Generation = () => {
 
+  const generation = { generationId: 696969, expiration: "2099-01-01" };
+
+  return (
+    <div>
+      <h3>Generation {Generation.generationId}. Expires on:</h3>
+      <h4>{new Date(generation.expiration).toString()}</h4>
+    </div>
+  );
 };
 
 export default Generation;
