8078b4b8133f26e76d5b61153ef9b53e2660db84
Ross Clettenberg <raclettenberg@gmail.com>
2024-03-03 18:42:21 -0600

    feat: add fetchGeneration func to Generation component

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -4,6 +4,23 @@ const Generation = () => {
 
   const [generation, setGeneration] = useState({ generationId: "696969", expiration: "2099-01-01" });
 
+  componentDidMount =() => {
+    this.fetchGeneration();
+  };
+
+  fetchGeneration = () => {
+    fetch("http://localhost:3000/generation")
+      .then(response => {
+        console.log("response", response);
+        response.json()
+      })
+      .then(json => {
+        console.log("json", json);
+        setGeneration(json.generation);
+      })
+      .catch(error => console.error("error", error));
+  };
+
   return (
     <div>
       <h3>Generation {generation.generationId}. Expires on:</h3>
