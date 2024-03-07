e549b422d0be6641e70627ee98fd85cf176175e6
Ross Clettenberg <raclettenberg@gmail.com>
2024-03-07 16:11:13 -0600

    feat: Bring in Bootstrap CDN link, set styles in index.css, and import index.css into index.js.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import Generation from "./components/Generation";
 import Dragon from "./components/Dragon";
 
+import "./index.css";
+
 const root = document.getElementById("root");
 createRoot(root).render(
   <div>
-    <h2>Dragon Stack from React</h2>
+    <h2>Dragon Stack</h2>
     <Generation />
     <Dragon />
   </div>
