a9c5cf7cbb7a69080651edfc9d91a253c95f681d
RossaMania <raclettenberg@gmail.com>
2024-05-01 12:55:55 -0500

    chore: Refactor setSession func in app/api/helper.js to update session ID in the database and set session string as a cookie on response.

--- backend/app/api/helper.js
+++ backend/app/api/helper.js
@@ -4,21 +4,23 @@ const { hash } = require("../account/helper.js");
 
 const setSession = ({ username, res }) => {
   return new Promise((resolve, reject) => {
+    const session = new Session({ username });
 
-  const session = new Session({ username });
+    const sessionString = session.toString();
 
-  const sessionString = session.toString();
-
-  AccountTable.updateSessionId({ sessionId: session.id, usernameHash: hash(username) });
-
-  res.cookie("sessionString", sessionString, {
-    expire: Date.now() + 3600000, // cookie expires in 1 hour
-    httpOnly: true, // cookie cannot be accessed by client side javascript
-    // secure: true // use with https cookie can only be sent over https connections
+    AccountTable.updateSessionId({
+      sessionId: session.id,
+      usernameHash: hash(username),
+    }).then(() => {
+      res.cookie("sessionString", sessionString, {
+        expire: Date.now() + 3600000, // cookie expires in 1 hour
+        httpOnly: true, // cookie cannot be accessed by client side javascript
+        // secure: true // use with https cookie can only be sent over https connections
+      });
+      resolve({ message: "Yay! Session created!"});
+    })
+    .catch(error => reject(error));
   });
-
-  })
-
-}
+};
 
 module.exports = { setSession };
