67e9d5cb347cd47dad91779ff9fbb29f13450851
RossaMania <raclettenberg@gmail.com>
2024-05-02 21:38:30 -0500

    feat: Create form elements in AuthForm component with React Bootstrap. Add login and signup buttons to AuthForm component. Set button attributes.

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel, Form } from "react-bootstrap";
 
 const AuthForm = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
-      <h2>Auth Form</h2>
+      <h2>Dragon Stack</h2>
+      <FormGroup>
+        <FormLabel>Username</FormLabel>
+        <FormControl
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <FormLabel>Password</FormLabel>
+        <FormControl
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </FormGroup>
+      <div>
+        <Button>Log In!</Button>
+        <span> or </span>
+        <Button>Sign Up!</Button>
+      </div>
+
     </div>
   );
 }
