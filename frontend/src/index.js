63083e221ac9ad583fc2dc94d4d21909dab5a15b
RossaMania <raclettenberg@gmail.com>
2024-05-02 21:27:57 -0500

    fix: Add import statement for the AuthForm in Root component. chore: Import Root component in root index.js and render Root in Provider.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,12 @@ import ReactDOM from "react-dom/client";
 import store from "./store";
 
 import "./index.css";
+import Root from "./components/Root";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    
+    <Root />
   </Provider>
 );
