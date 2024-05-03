2062b6824012ca192e7011968feff9151b7d30d3
RossaMania <raclettenberg@gmail.com>
2024-05-03 09:59:56 -0500

    chore: add authSliceReducer to Redux store.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,29 +1,18 @@
 import { apiSlice } from "./slices/apiSlice";
 import { configureStore } from "@reduxjs/toolkit";
-import reducerWithLogs from "./slices/generationSlice";
+import generationReducer from "./slices/generationSlice";
 import dragonReducer from './slices/dragonSlice';
+import authSliceReducer from './slices/authSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    generation: reducerWithLogs,
+    generation: generationReducer,
     dragon: dragonReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
-
-// import { configureStore } from "@reduxjs/toolkit";
-
-
-// const store = configureStore({
-//   reducer: {
-//     [apiSlice.reducerPath]: apiSlice.reducer
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware().concat(apiSlice.middleware)
-// });
-
-// export default store;
