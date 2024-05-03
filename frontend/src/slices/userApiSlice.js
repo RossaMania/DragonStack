53b8bb248f1c05e67aa507a5850f3115809f2ea9
RossaMania <raclettenberg@gmail.com>
2024-05-03 09:47:45 -0500

    Add usersApiSlice to handle login, registration, and logout functionality

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,27 @@
+import { BASE_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: BASE_URL,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: BASE_URL,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: BASE_URL,
+        method: "POST",
+      }),
+    }),
+}),
+})
