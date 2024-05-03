0aa762faf0a6e8189d34d8dc784680e86929077b
RossaMania <raclettenberg@gmail.com>
2024-05-02 21:22:52 -0500

    feat: Create AuthForm component with placeholder text. Refactor Root component to conditionally render AuthForm instead of ComponentTwo.

---
+++ frontend/src/components/AuthForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthForm = () => {
+  return (
+    <div>
+      <h2>Auth Form</h2>
+    </div>
+  );
+}
+
+export default AuthForm;
--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./Home";
 
 const Root = () => {
-  return true ? <Home /> : <ComponentTwo />;
+  return true ? <Home /> : <AuthForm />;
 }
 
 export default Root;
