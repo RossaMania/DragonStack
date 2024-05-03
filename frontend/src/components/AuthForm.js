9bb79ead0a1c9fb316268663a354594c4c89a0cb
RossaMania <raclettenberg@gmail.com>
2024-05-02 21:52:33 -0500

    chore: Refactor AuthForm component to use separate funcs for updating username and password fields and capture event target values. Add login and signup funcs with console logs.

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -6,6 +6,22 @@ const AuthForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const updateUsername = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const updatePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const login = () => {
+    console.log("login", { username, password });
+  }
+
+  const signup = () => {
+    console.log("signup", { username, password });
+  }
+
   return (
     <div>
       <h2>Dragon Stack</h2>
@@ -15,20 +31,20 @@ const AuthForm = () => {
           type="text"
           value={username}
           placeholder="username"
-          onChange={(event) => setUsername(event.target.value)}
+          onChange={updateUsername}
         />
         <FormLabel>Password</FormLabel>
         <FormControl
           type="password"
           value={password}
           placeholder="password"
-          onChange={(event) => setPassword(event.target.value)}
+          onChange={updatePassword}
         />
       </FormGroup>
       <div>
-        <Button>Log In!</Button>
+        <Button onClick={login}>Log In!</Button>
         <span> or </span>
-        <Button>Sign Up!</Button>
+        <Button onClick={signup}>Sign Up!</Button>
       </div>
 
     </div>
