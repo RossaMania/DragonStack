78aef3b1dd45ed4fb26e4885f5eb3a76288d2e1e
RossaMania <raclettenberg@gmail.com>
2024-06-26 21:58:57 -0500

    chore: Refactor AccountDragons component. Improve loading state by conditionally rendering Account Dragons component. No dragons throws a message.

--- frontend/src/components/AccountDragons.js
+++ frontend/src/components/AccountDragons.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import Loader from "./Loader";
-import { useFetchAccountDragonsQuery } from "../slices/dragonApiSlice";
+import { useFetchAccountDragonsQuery } from '../slices/dragonApiSlice';
 
 const AccountDragons = () => {
-  const { data = { dragons: [] }, error, isLoading } = useFetchAccountDragonsQuery();
+  const { data, error, isLoading } = useFetchAccountDragonsQuery();
 
   if (isLoading) return <Loader />;
   if (error) return <p>Error: {error.message}</p>;
@@ -12,12 +12,43 @@ const AccountDragons = () => {
     <div>
       <h1>Account Dragons</h1>
       <ul>
-        {data.dragons.map((dragon) => (
-          <li key={dragon.id}>{dragon.name}</li>
-        ))}
+        {data && data.dragons ? (
+          data.dragons.map((dragon) => (
+            <li key={dragon.id}>{dragon.name}</li>
+          ))
+        ) : (
+          <p>No dragons available.</p>
+        )}
       </ul>
     </div>
   );
 };
 
 export default AccountDragons;
+
+
+
+
+// import React from "react";
+// import Loader from "./Loader";
+// import { useFetchAccountDragonsQuery } from "../slices/dragonApiSlice";
+
+// const AccountDragons = () => {
+//   const { data = { dragons: [] }, error, isLoading } = useFetchAccountDragonsQuery();
+
+//   if (isLoading) return <Loader />;
+//   if (error) return <p>Error: {error.message}</p>;
+
+//   return (
+//     <div>
+//       <h1>Account Dragons</h1>
+//       <ul>
+//         {data.dragons.map((dragon) => (
+//           <li key={dragon.id}>{dragon.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default AccountDragons;
