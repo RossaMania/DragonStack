626806cc19036e08924844befd13a1919cbaa4f2
RossaMania <raclettenberg@gmail.com>
2024-09-01 20:07:37 -0500

    feat: call fetAccountInfo hook with a useEffect to fetch account info in AccountInfo component.

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
 import { useFetchAccountInfoQuery } from "../slices/usersApiSlice"
+import Loader from "./Loader"
 
 const AccountInfo = () => {
   const { data: accountInfo, error, isLoading } = useFetchAccountInfoQuery()
+
+  if (isLoading) return <Loader />
+
+  if (error) return <div>Error: {error.message}</div>
+
+  useEffect(() => {
+    fetchAccountInfo()
+  }, []) // Empty array means this effect runs only once
 
   return (
     <div>
