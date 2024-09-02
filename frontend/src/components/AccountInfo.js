dd49a8f7535d4943cedbe56106b371d6ef3f8a83
RossaMania <raclettenberg@gmail.com>
2024-09-01 20:48:31 -0500

    refactor: Simplify AccountInfo component. Removed unnecessary useEffect in AccountInfo.js to streamline state handling with RTK Query. Improved error and loading state management for a cleaner and more readable component.Simplified authSlice.js by enhancing clarity.

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,28 +1,19 @@
-import { useEffect } from "react"
-import { useFetchAccountInfoQuery } from "../slices/usersApiSlice"
-import { useDispatch, useSelector } from "react-redux";
-import Loader from "./Loader"
+import { useFetchAccountInfoQuery } from "../slices/usersApiSlice";
+import Loader from "./Loader";
 
 const AccountInfo = () => {
-  const { data: accountInfo, error, isLoading } = useFetchAccountInfoQuery()
+  const { data: accountInfo, error, isLoading } = useFetchAccountInfoQuery();
 
-
-  useEffect(() => {
-    
-  }, []) // Empty array means this effect runs only once
-
-  if (isLoading) return <Loader />
-
-  if (error) return <div>Error: {error.message}</div>
-
+  if (isLoading) return <Loader />;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-    <h3>Account Info</h3>
-    <div>Username: {accountInfo.username}</div>
-    <div>Balance: {accountInfo.balance}</div>
+      <h3>Account Info</h3>
+      <div>Username: {accountInfo.info.username}</div>
+      <div>Balance: {accountInfo.info.balance}</div>
     </div>
-  )
-}
+  );
+};
 
-export default AccountInfo
+export default AccountInfo;
