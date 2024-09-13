645d16a600c2bed474b1d3b236626277cb75e6c3
RossaMania <raclettenberg@gmail.com>
2024-09-12 20:36:00 -0500

    feat: Add dragon ID display to PublicDragonRow component.

--- frontend/src/components/PublicDragonRow.js
+++ frontend/src/components/PublicDragonRow.js
@@ -4,6 +4,7 @@ const PublicDragonRow = ({ dragon }) => {
   return (
     <div>
       <div>{dragon.nickname}</div>
+      <div>{dragon.dragonId}</div>
       <DragonAvatar dragon={dragon} />
       <div>{dragon.saleValue}</div>
     </div>
