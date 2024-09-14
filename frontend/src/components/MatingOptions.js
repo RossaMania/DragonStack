70c6a6bd95e6ed161da665335d4702ba9779e006
RossaMania <raclettenberg@gmail.com>
2024-09-14 16:47:24 -0500

    refactor: Add check for available dragons in MatingOptions component

--- frontend/src/components/MatingOptions.js
+++ frontend/src/components/MatingOptions.js
@@ -1,6 +1,10 @@
 import { Button } from "react-bootstrap"
 
 const MatingOptions = ({ accountDragons }) => {
+  if (!accountDragons || !accountDragons.dragons) {
+    return <div>No dragons available for mating.</div>;
+  }
+
   return (
     <div>
       <h4>Pick one of your dragons to mate with!</h4>
